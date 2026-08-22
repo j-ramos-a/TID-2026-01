01e769cac356da5dbcfc880ef225672ed210d5b3
Joaquin Ramos <joaquin.ramos.arevalo@gmail.com>
2026-08-22 16:58:29 -0400

    Ejercicio de adivinanza mejorado

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use std::io;
+use std::cmp::Ordering;
+
+fn main() {
+    println!("Guess the number!");
+
+    let aleatorio:u32 = rand::random_range(0..=100);
+
+    println!("Please input your guess. {aleatorio}");
+
+    let mut guess = String::new();
+
+    if let Err(_) = io::stdin().read_line(&mut guess) {
+        println!("Error al leer de la consola");
+        return;
+    }
+
+    // 2. Declaramos `guess` en el ámbito principal de main()
+    let guess: u32 = match guess.trim().parse() {
+        Ok(adivinado) => adivinado,
+        Err(_) => {
+            println!("Ingrese un número válido");
+            return; 
+        }
+    };
+    println!("You guessed: {guess}");
+
+
+    match guess.cmp(&aleatorio) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
+    
+}
